75aa792092c930d90413baaf8df51c7585f7a974
letthelightin <realmer@gmail.com>
2014-06-01 16:50:31 -0700

    Serious update 2 of 2 - better defined flow of project

--- src/Block.java
+++ src/Block.java
@@ -1,38 +1,124 @@
-/**This class is a Constructor for object Block of the Java 2048 Project. 
- * The variables of Block objects include
- * ~ int value (numeric value & displayed character of Block)
- * ~ int catalyst (those that combine w/ this Block to change/advance)
- *
- * Methods will 
- * ~ return all variables individually
- * ~ set all variables individually
- *
- * @author Daniel Retta 
- * @version 2014.05.14
- */
-public class Block {
-    // instance variables - replace the example below with your own
-    private int value;
-    private int catalyst;
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+public class Block extends Application {
+
+    private int value;      // value of the block
+
+    private int width;      // width of the block pixels
+    private int height;     // height of the block pixels
+
+    private double strokeWidth;     // strength of the stroke
+    // around the rectangle
+
+    private Color block;            // color of the block
+    private Color stroke;           // color of the border
+    private Color label;            // color of the label
+
+    private Pane image;            // group of javafx components
+
+    public static void main(String args[]) {
+        // this class can be run to provide an example
+        // of a single block being drawn
+        launch(args);
+    }
+
+    public void start(Stage primaryStage) throws Exception {
+
+        primaryStage.setTitle("example " + getClass().getSimpleName());
+
+        Group root = new Group();
+
+        Scene scene = new Scene(root,400,400,Color.WHITE);
+        /////
+
+
+        root.getChildren().add(this.drawing(50,50,300,300));
+
+
+
+        /////
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
 
     public Block() {
         this.value = 0;
-        this.catalyst = 1;
+        this.width = 80;
+        this.height = 80;
+        this.strokeWidth = 3;
+        this.block = Color.WHITE;
+        this.stroke = Color.BLACK;
+        this.label = Color.BLACK;
     }
 
-    public Block(int value, int catalyst){
-        this.value = value;
-        this.catalyst = catalyst;
+    public Block(int value) {
+        this.value = 0;
+        this.strokeWidth = 3;
+        this.block = Color.WHITE;
+        this.stroke = Color.BLACK;
+        this.label = Color.BLACK;
     }
 
-    //these methods alter individual variables of this Block
-    public void value(int value){this.value = value;}
+    // SET Constructors for private variables of this Block
+    public void value(int value) { this.value = value; }
+    public void width(int width) { this.width = width; }
+    public void height(int height) { this.height = height; }
+    public void strokeWidth(double strokeWidth) { this.strokeWidth = strokeWidth; }
+    public void block(Color color) { this.block = color; }
+    public void stroke(Color color) { this.stroke = color; }
+    public void label(Color color) { this.label = color; }
 
-    public void catalyst(int catalyst){this.catalyst = catalyst;}
+    // GET Constructors for private variables of this Block
+    public int value() { return this.value; }
+    public int width() { return this.width; }
+    public int height() { return this.height; }
+    public double strokeWidth() { return this.strokeWidth; }
+    public Color block() { return this.block; }
+    public Color stroke() { return this.stroke; }
+    public Color label() { return this.label; }
+    public Pane image() { return this.image; }
 
-    //these methods return individual variables of this Block
-    public int value(){return this.value;}
+    public Pane drawing(int x, int y, int width, int height) {
 
-    public int catalyst(){return this.catalyst;}
+        this.image = new Pane();
+
+        Rectangle rectangle = new Rectangle(x, y, width, height);
+        rectangle.setSmooth(true);
+        rectangle.setArcHeight(30*width/100);
+        rectangle.setArcWidth(30*width/100);
+        rectangle.setStroke(Color.BLACK);
+        rectangle.setFill(Color.WHITE);
+        rectangle.setStrokeWidth(3*width/100);
+        rectangle.setManaged(true);
+
+        this.image.getChildren().add(rectangle);
+
+        Label label = new Label("" + this.value);
+        label.setLayoutX(x + 40*width/100);
+        label.setLayoutY(y + 30*height/100);
+        label.setFont(Font.font("Calibri", 50*width/100));
+        label.setTextFill(this.label);
+        label.setManaged(true);
+
+        this.image.getChildren().add(label);
+
+        this.image.setManaged(true);
+
+        return this.image;
+    }
 
 }
+
+
+
+
+
--- src/Board.java
+++ src/Board.java
@@ -1,9 +1,9 @@
 /**This class is a Constructor for object Board of the Java 2048 Project. 
  * The variables of Board objects include
  *
- * int blockCountSquared (quantity of rows&columns of Block objects) 
- * int boardPixelSizeSquared (quantity of pixels wide&high of Board objects)
- * int blockPixelSizeSquared (quantity of pixels wide&high of Board objects)
+ * int squarePlaces (quantity of rows&columns of Block objects) 
+ * int squarePixels (quantity of pixels wide&high of Board objects)
+ * int squarePixels / squarePlaces (quantity of pixels wide&high of Board objects)
 
  * @author Daniel Retta 
  * @version 2014.05.14
@@ -11,61 +11,97 @@
 
 //graphics libraries
 import javafx.application.Application;
-import javafx.geometry.Point2D;
+import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
-import javafx.stage.Stage;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
 
-public class Board {
+public class Board extends Application {
+
     private Block[][] board;
-    private int blockCountSquared;
-    private int boardPixelSizeSquared;
-    private int blockPixelSizeSquared;
 
-    public Board() {
-        board = new Block[0][0];
+    private int squarePlaces;
+    private int squarePixels;
 
-        blockCountSquared = 0;
-        boardPixelSizeSquared = 0;
+    private int spacer;
+
+    public static void main(String args [] ) {
+        // this class can be run to provide an example
+        // of an example Board with a randomly placed block
+        // with a value of 2
+        launch(args);
     }
 
-    public void defineBoard(int blockCountSquared, int boardPixelSizeSquared){
-        this.blockCountSquared = blockCountSquared;
-        this.boardPixelSizeSquared = boardPixelSizeSquared;
+    public void start(Stage primaryStage) throws Exception {
 
-        board = new Block[this.blockCountSquared][this.blockCountSquared];
+        primaryStage.setTitle(getClass().getSimpleName());
+
+        Group root = new Group();
+
+        Scene scene = new Scene(root,400,400, Color.LIGHTPINK);
+        //////
+
+        Board board = new Board(4,400);
+
+        this.drawBackground(root);
+        //this.drawBlocks(root);
+
+        ///////
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+    public Board() {
+        // default values of Board presumes a 4x4 board of 400 pixels wide
+        this.squarePlaces = 4;
+        this.squarePixels = 400;
+
+        this.board = new Block[this.squarePlaces][this.squarePlaces];
         int j = 0;
         for (int i = 0; i < this.board.length; i++) {
             for (j = 0; j < this.board[0].length; j++) {
-                board[i][j] = new Block(0,1);
+                this.board[i][j] = new Block(0);
             }
             j = 0;
         }
     }
 
-    public Block block(int x, int y) { //actual block address
+    public Board(int squarePlaces, int squarePixels){
+        this.squarePlaces = squarePlaces;
+        this.squarePixels = squarePixels;
+
+        this.board = new Block[this.squarePlaces][this.squarePlaces];
+        int j = 0; //
+        for (int i = 0; i < this.board.length; i++) {
+            for (j = 0; j < this.board[0].length; j++) {
+                this.board[i][j] = new Block(0);
+            }
+            j = 0;
+        }
+    }
+
+    public Object block(int x, int y) { //actual block address
         return this.board[x][y];
     }
 
     public Block block(int b) { //unique integer for each block address
-        int x = b / this.blockCountSquared();
-        int y = b % this.blockCountSquared();
+        int x = b / this.squarePlaces();
+        int y = b % this.squarePlaces();
 
         return this.board[x][y];
     }
 
-    public int blockCountSquared(){return this.blockCountSquared;}
+    public int squarePlaces(){return this.squarePlaces;}
 
-    public int boardPixelSizeSquared(){return this.boardPixelSizeSquared;}
-
-    public int blockPixelSizeSquared(){return this.blockPixelSizeSquared;}
+    public int squarePixels(){return this.squarePixels;}
 
     public boolean checkIfFull() {
-        for (int i = 0; i < Math.pow(this.blockCountSquared,2); i++){
+        for (int i = 0; i < Math.pow(this.squarePlaces,2); i++){
             if (block(i).value() == 0){
                 return false;
             }
@@ -73,30 +109,41 @@ public class Board {
         return true;
     }
 
-    public void draw(GraphicsContext gC){
+    public void drawBackground(Group root){
+        int x;      int y;
+        int width;  int height;
 
-        int j = 0;
-        for (int i = 0; i < this.board.length; i++) {
-            for (j = 0; j < this.board[0].length; j++) {
+        for (int i = 0; i < this.squarePlaces*this.squarePlaces; i++) {
 
-                gC.fillRect(                                    // draw each block
-                        i * this.blockPixelSizeSquared + 5,     // x
-                        j * this.blockPixelSizeSquared + 5,     // y
+            // provide an identifying x and y coordinate for our board grid
+            x = i / this.squarePlaces;
+            y = i % this.squarePlaces;
 
-                        i * this.blockPixelSizeSquared - 5,     // width
-                        j * this.blockPixelSizeSquared - 5      // height
-                );
-            }
-            j = 0;
+            //provide the width and height of a place rectangle
+            width = this.squarePixels/this.squarePlaces - this.spacer * 2;
+            height = this.squarePixels/this.squarePlaces - this.spacer * 2;
+
+            // provide an x and y coordinate for our pixel grid
+            x = x * this.squarePixels/this.squarePlaces + this.spacer;
+            y = y * this.squarePixels/this.squarePlaces + this.spacer;
+
+            Rectangle rectangle = new Rectangle(x,y,width,height);
+            rectangle.setSmooth(true);
+            rectangle.setArcHeight(50);
+            rectangle.setArcWidth(50);
+            rectangle.setStroke(Color.BLACK);
+            rectangle.setFill(Color.WHITE);
+            rectangle.setStrokeWidth(2);
+
+            root.getChildren().add(rectangle);
         }
-
     }
 
     public void generateNewBlock(){
         // This method randomly selects a block with
         // a current value of 0 and sets that value to 1
 
-        double c = this.blockCountSquared-1;
+        double c = this.squarePlaces-1;
 
         double i = 0;
         double j = 0;
